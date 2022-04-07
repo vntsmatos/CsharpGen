cc6b359900566493add975c68ef19b5224428b05
vntsmatos <ovinidev@gmail.com>
2022-04-07 15:59:28 -0300

    @vntsmatos [exercios aula 07.04]

---
+++ Aula07.04/Aula07.04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula07._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var pessoa =ImprimeNome("Vinicius Santos");
+
+            Console.WriteLine("Nome é : " + pessoa.Item2);
+            Console.WriteLine("Tamanho do nome é : "+ pessoa.Item2);
+            
+        }
+        public static (string, int) ImprimeNome(string nome = default)
+        {
+            return($"O nome é: {nome}",nome.Length);
+        }
+    }
+}
---
+++ CalcExGenEnq/CalcExGenEnq/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace CalcExGenEnq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool desligar = false;
+
+
+            while (!desligar) //! esclamacao nega a afrimacao enquanto nao desligar...
+            {
+                Console.WriteLine("Calculadora");
+                Console.Write("digite o primeiro numero: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("digite o operador: ");
+                string operador = Console.ReadLine();
+
+                Console.Write("digite o segundo numero: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                switch (operador)
+                {
+                    case "+":
+                        Console.WriteLine("resultado: " + somar(num1, num2));
+                        break;
+                    case "-":
+                        Console.WriteLine("resultado: " + subtrair(num1, num2));
+                        break;
+                    case "*":
+                        Console.WriteLine("resultado: " + multiplicar(num1, num2));
+                        break;
+                    case "/":
+                        Console.WriteLine("resultado: " + dividir(num1, num2));
+                        break;
+                }
+
+
+                Console.WriteLine("Deseja desligar? ");
+                string condicao = Console.ReadLine();//NAO RECEBE PARAMETRO SÓ RETORNA
+                if(condicao == "Sim")//Aqui tem que digitar Sim com S maiusculo 
+                {
+                    desligar = true;
+                }
+                else
+                {
+                    desligar = false;
+                }
+
+            }
+
+            
+
+        }
+
+        public static string somar(int num1, int num2)
+        {
+            return (num1 + num2).ToString();
+        }
+        public static string subtrair(int num1, int num2)
+        {
+            return (num1 - num2).ToString();
+        }
+        public static string multiplicar(int num1, int num2)
+        {
+            return (num1 * num2).ToString();
+        }
+        public static string dividir(int num1, int num2)
+        {
+            if (valorDividendoMenorOuIgualAZero(num2))
+            {
+                return "Não é possivel dividir por zero ou menor que zero";
+            }
+            return (num1 / num2).ToString();
+
+            bool valorDividendoMenorOuIgualAZero(int num2)
+            {
+                if (num2 <= 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
