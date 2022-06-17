8b0126181cb4481b8c1baefa498c88221d48360e
vntsmatos <ovinidev@gmail.com>
2022-06-16 22:44:36 -0300

    @vntsmatos[Task:Verificacao Par ou Impar]{16/06/2022}

---
+++ ExercicioParImpar/ExercicioParImpar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Desenvolva um algoritmo em C# que solicite a entrada de um número e calcule se o número é par ou impar.
+
+namespace ExercicioParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Por favor digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            //Para verificar se é par ou impar basta analisar se é ddivisível por 2
+            //Se for divisível por 2 ==> É par
+            //Se não for divisível por 2 ==> Não é par
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O número é par!");
+            }
+            else
+            {
+                Console.WriteLine("O número é impar");
+            }
+
+            //Aguarda que uma tecla seja pressionada
+            Console.ReadKey();
+        }
+    }
+}
